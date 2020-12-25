cb37ffe842207367f68e1d92a805b0aab9b1f51f
Micah Caffey <mrcaffey@gmail.com>
2020-12-25 12:55:57 -0600

    basic routes

---
+++ src/components/Coffee.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Coffee extends React.Component {
+    render() {
+    return (
+        <div>
+            <p>Coffee shop component</p>
+        </div>
+    )
+    }
+}
+
+export default Coffee;
---
+++ src/components/Merch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Merch extends React.Component {
+    render() {
+    return (
+        <div>
+            <p>Merch component</p>
+        </div>
+    )
+    }
+}
+
+export default Merch;
